f486764835c40932671e05d4f2bd806b62526be4
chen hui run <chenhr@lxslc710.ihep.ac.cn>
2021-04-14 22:11:57 +0800

    complete variationPlot; add addShowerSys.sh

--- shape_sys/plotFit/variationPlot.cxx
+++ shape_sys/plotFit/variationPlot.cxx
@@ -26,16 +26,50 @@ void variationPlot(TString sys){
   TFile *fdown = new TFile(workarea+"/ws_"+sys+"__1down_m00.root", "read");
 
   for(auto cat : catCuts){
-    if(cat.first != "TT_b2") continue;
+    if(cat.first != "TT_b1") continue;
     TCanvas *canv = new TCanvas("c", "canvas", 800, 600);
-  
+    TPad *pad1 = new TPad("pad1", "pad1", 0, 0., 0.7, 1.0);
+    TPad *pad2 = new TPad("pad2", "pad2", 0.7, 0.5, 1, 1.0);
+    TPad *pad3 = new TPad("pad3", "pad3", 0.7, 0., 1, 0.5);
+
+    TLegend* lg = new TLegend(0.4, 0.75, 0.65, 0.9);
+
+    pad1->Draw();
+    pad1->cd();
+
     RooWorkspace *wsnom = (RooWorkspace*)fnom->Get("ws");
+    wsnom->var("m_yy")->setRange("SR", 118000, 132000);
     RooPlot* myy_frame = wsnom->var("m_yy")->frame(Title("fit to m_yy distribution"));
-    wsnom->data(("dh_myy_sig_"+cat.first).Data())->plotOn(myy_frame);
-    //wsnom->Print();
-    wsnom->pdf(("sig_"+cat.first).Data())->plotOn(myy_frame);
+    wsnom->data(("dh_myy_sig_"+cat.first).Data())->plotOn(myy_frame, MarkerSize(0.1), DataError(RooAbsData::None), Binning(550));
+    wsnom->pdf(("sig_"+cat.first).Data())->plotOn(myy_frame, LineWidth(1), Range(118000, 132000));
+  
+    RooWorkspace *wsup = (RooWorkspace*)fup->Get("ws");
+    //wsup->data(("dh_myy_sig_"+cat.first).Data())->plotOn(myy_frame, MarkerSize(0.25));
+    wsup->pdf(("sig_"+cat.first).Data())->plotOn(myy_frame, LineStyle(kDashed), LineColor(kGreen), LineWidth(1));
+  
+    RooWorkspace *wsdown = (RooWorkspace*)fdown->Get("ws");
+    //wsdown->data(("dh_myy_sig_"+cat.first).Data())->plotOn(myy_frame, MarkerSize(0.25));
+    wsdown->pdf(("sig_"+cat.first).Data())->plotOn(myy_frame, LineStyle(kDashed), LineColor(kViolet), LineWidth(1), Range(118000, 132000));
 
     myy_frame->Draw();
+
+    canv->cd();
+
+    pad2->Draw();
+    pad2->cd();
+    RooPlot* myy_frame2 = wsnom->var("m_yy")->frame(Title(sys+"_up"));
+    wsup->data(("dh_myy_sig_"+cat.first).Data())->plotOn(myy_frame2, MarkerSize(0.25));
+    wsup->pdf(("sig_"+cat.first).Data())->plotOn(myy_frame2, LineStyle(kDashed), LineColor(kGreen), LineWidth(1));
+    myy_frame2->Draw();
+
+    canv->cd();
+
+    pad3->Draw();
+    pad3->cd();
+    RooPlot* myy_frame3 = wsnom->var("m_yy")->frame(Title(sys+"_down"));
+    wsup->data(("dh_myy_sig_"+cat.first).Data())->plotOn(myy_frame3, MarkerSize(0.25));
+    wsup->pdf(("sig_"+cat.first).Data())->plotOn(myy_frame3, LineStyle(kDashed), LineColor(kViolet), LineWidth(1));
+    myy_frame3->Draw();
 
     canv->SaveAs("plot_"+sys+"_m00_"+cat.first+".png");
   }
---
+++ yield_sys/TheorySys/addShowerSys.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+
+inpath=/scratchfs/atlas/chenhr/atlaswork/VBF_CP/syst/yield_sys/TheorySys/showerSys
+
+transfer(){
+  inf=$1
+  sampleID=$2
+  d_tilde=$3
+  bdtCat=$4
+
+  sampleName=$(echo ${inf} | cut -d _ -f 1)
+
+  sed -i '/shower*,/d' ./mag_theory_${sampleID}_${d_tilde}_${bdtCat}_b1.csv
+  sed -i '/shower*,/d' ./mag_theory_${sampleID}_${d_tilde}_${bdtCat}_b2.csv
+  sed -i '/shower*,/d' ./mag_theory_${sampleID}_${d_tilde}_${bdtCat}_b3.csv
+  sed -i '/shower*,/d' ./mag_theory_${sampleID}_${d_tilde}_${bdtCat}_b4.csv
+  sed -i '/shower*,/d' ./mag_theory_${sampleID}_${d_tilde}_${bdtCat}_b5.csv
+  sed -i '/shower*,/d' ./mag_theory_${sampleID}_${d_tilde}_${bdtCat}_b6.csv
+
+  lines=$(cat ${inpath}/$inf | sed 's/ //g'); #echo $lines
+
+  for l in $lines
+  do
+
+    sysname=$(echo $l | cut -d , -f 1)
+
+    b1=$(echo $l | cut -d , -f 2)
+    b2=$(echo $l | cut -d , -f 3)
+    b3=$(echo $l | cut -d , -f 4)
+    b4=$(echo $l | cut -d , -f 5)
+    b5=$(echo $l | cut -d , -f 6)
+    b6=$(echo $l | cut -d , -f 7)
+
+    echo "${sysname}_${sampleName},${b1}" >> mag_theory_${sampleID}_${d_tilde}_${bdtCat}_b1.csv
+    echo "${sysname}_${sampleName},${b2}" >> mag_theory_${sampleID}_${d_tilde}_${bdtCat}_b2.csv
+    echo "${sysname}_${sampleName},${b3}" >> mag_theory_${sampleID}_${d_tilde}_${bdtCat}_b3.csv
+    echo "${sysname}_${sampleName},${b4}" >> mag_theory_${sampleID}_${d_tilde}_${bdtCat}_b4.csv
+    echo "${sysname}_${sampleName},${b5}" >> mag_theory_${sampleID}_${d_tilde}_${bdtCat}_b5.csv
+    echo "${sysname}_${sampleName},${b6}" >> mag_theory_${sampleID}_${d_tilde}_${bdtCat}_b6.csv
+  done
+}
+
+ds=$(ls mag_theory_346214_*_TT_b1.csv | cut -d _ -f 4)
+cats=$(ls mag_theory_346214_m00_*_b1.csv | cut -d _ -f 5)
+
+for c in ${cats};do
+  bdtCat=${c}
+
+  #ggF
+  inf=ggF_showerSys_${c}_d0.txt
+  sampleID=343981
+  d_tilde=SM
+  echo "$sampleID $d_tilde ${bdtCat}"
+  transfer $inf $sampleID $d_tilde ${bdtCat}
+
+  #VBF
+  inf=VBF_showerSys_${c}_d0.txt
+  sampleID=346214
+  for d in ${ds};do
+    d_tilde=${d}
+    echo "$sampleID $d_tilde ${bdtCat}"
+    transfer $inf $sampleID $d_tilde ${bdtCat}
+  done
+done
