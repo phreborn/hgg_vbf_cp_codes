d958463e0d6a6c7d2dc9dcf512eba460bb37eb37
chen hui run <huirun@lxslc712.ihep.ac.cn>
2021-07-18 22:10:51 +0800

    add getMag_jd.cxx

--- yield_sys/compile.sh
+++ yield_sys/compile.sh
@@ -11,3 +11,6 @@
 #include lib in x86_64-centos7-gcc8-opt, better choose OS CentOS7 in condor mode
 g++ -I. `root-config --cflags --libs` -I/cvmfs/sft.cern.ch/lcg/releases/LCG_97a/vdt/0.4.3/x86_64-centos7-gcc8-opt/include/ -o getMag.exe getMag.cxx
 chmod +x getMag.exe
+
+g++ -I. `root-config --cflags --libs` -I/cvmfs/sft.cern.ch/lcg/releases/LCG_97a/vdt/0.4.3/x86_64-centos7-gcc8-opt/include/ -o getMag_jd.exe getMag_jd.cxx
+chmod +x getMag_jd.exe
