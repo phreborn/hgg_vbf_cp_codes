468e7a05b549da7d4a9fe51aa0758f63f44082b7
chen hui run <huirun@lxslc716.ihep.ac.cn>
2021-10-02 14:39:29 +0800

    shape_sys/genSubmit.sh: remove Collect_*_* before getMag

--- shape_sys/genSubmit.sh
+++ shape_sys/genSubmit.sh
@@ -21,6 +21,7 @@ for init in ${sequence[@]};do
   echo "source \${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh" >> exe_${jobName}.sh
   echo "lsetup \"root 6.20.06-x86_64-centos7-gcc8-opt\"" >> exe_${jobName}.sh
   #echo "root -b -l -q getMag.cxx\(${init},${fin}\) | tee -a submit_${jobName}/log.fitStatus" >> exe_${jobName}.sh
+  echo "rm -r /publicfs/atlas/atlasnew/higgs/hgg/chenhr/vbfcp/syst/shape/csv/${jobName}" >> exe_${jobName}.sh
   echo "./bin/getMag.exe ${init} ${fin} | tee -a submit_${jobName}/log.fitStatus" >> exe_${jobName}.sh
 
   chmod +x exe_${jobName}.sh
