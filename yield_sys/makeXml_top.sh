b474803eb8e6614e093ca83ec5f3a10733951e0a
chen hui run <huirun@lxslc708.ihep.ac.cn>
2021-10-06 18:03:18 +0800

    inject test

--- yield_sys/makeXml_top.sh
+++ yield_sys/makeXml_top.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-injectTest=1
+injectTest=0
 injectPoint=m03
 
 d_tilde=$(ls csv/ | grep TT_b3 | grep -v SM | cut -d '_' -f 4)
@@ -9,6 +9,7 @@ bin=$(ls csv/ | grep m01 | cut -d '_' -f 5 | cut -d '.' -f 1)
 
 #cats=$(ls /scratchfs/atlas/chenhr/atlaswork/VBF_CP/calcBDT/outputs/mc16a/ | grep  343981_ggF_Nominal | cut -d _ -f 4 | cut -d . -f 1)
 cats=$(cat ../../nom_WS/cats.cfg | grep -v "#" | grep ":" | cut -d ":" -f 1)
+#cats=$(cat ../../nom_WS/cats.cfg | grep -v "#" | grep ":" | cut -d ":" -f 1 | grep LL)
 
 echo $d_tilde
 echo $bin
@@ -36,14 +37,20 @@ for d in $d_tilde;do
     echo "  <Input>config/vbf_cp_${d}/channel/category_OO_${cat}.xml</Input>" >> $out_xml
   done
 
-  echo "  <POI>mu_VBF_RW,mu_VBF_SM,mu_VBF_${injectPoint},mu_ggH,mu_ggH_SM,mu_ggH_${injectPoint},mu</POI>" >> $out_xml
+  if [ $injectTest -eq 1 ];then
+    echo "  <POI>mu_VBF_RW,mu_VBF_SM,mu_VBF_${injectPoint},mu_ggH,mu_ggH_SM,mu_ggH_${injectPoint},mu</POI>" >> $out_xml
+  else
+    echo "  <POI>mu_VBF_RW,mu_VBF_SM,mu_ggH,mu_ggH_SM,mu</POI>" >> $out_xml
+  fi
   echo "" >> $out_xml
 
   #echo "  <Asimov Name=\"asimovData_SM_noFit\"   Setup=\"mu_VBF_RW=0,mu_VBF_SM=1,mu=1\"     Action=\"raw:fixall:genasimov:float:savesnapshot\" SnapshotNuis=\"nominalNuis_SM_noFit\" SnapshotGlob=\"nominalGlob_SM_noFit\"/>" >> $out_xml # PE b, c
   echo "  <Asimov Name=\"asimovData_B_blind\"   Setup=\"mu=0\"     Action=\"fixsyst:fit:genasimov:float:savesnapshot\" SnapshotNuis=\"nominalNuis_B_blindFit\" SnapshotGlob=\"nominalGlob_B_blindFit\"/>" >> $out_xml
-  echo "  <Asimov Name=\"asimovData_SB_SM\"  Setup=\"mu=1,mu_VBF_RW=0,mu_ggH=0,mu_VBF_${injectPoint}=0,mu_ggH_${injectPoint}=0\"     Action=\"genasimov:reset\"/>" >> $out_xml
   if [ $injectTest -eq 1 ];then
+    echo "  <Asimov Name=\"asimovData_SB_SM\"  Setup=\"mu=1,mu_VBF_RW=0,mu_ggH=0,mu_VBF_${injectPoint}=0,mu_ggH_${injectPoint}=0\"     Action=\"genasimov:reset\"/>" >> $out_xml
     echo "  <Asimov Name=\"asimovData_SB_${injectPoint}\"  Setup=\"mu=1,mu_VBF_RW=0,mu_ggH=0,mu_VBF_SM=0,mu_ggH_SM=0\"     Action=\"genasimov:reset\"/>" >> $out_xml
+  else
+    echo "  <Asimov Name=\"asimovData_SB_SM\"  Setup=\"mu=1,mu_VBF_RW=0,mu_ggH=0\"     Action=\"genasimov:reset\"/>" >> $out_xml
   fi
   echo "" >> $out_xml
 
