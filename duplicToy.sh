24a9478d05cf3436f3b0ecbb46d511a2c1606f94
chen hui run <huirun@lxslc713.ihep.ac.cn>
2022-02-24 22:04:45 +0800

    minor

--- duplicToy.sh
+++ duplicToy.sh
@@ -6,7 +6,8 @@ if [ $1 == "-i" ];then itoy=$2;fi
 d_tilde=$(cat Dtilde | grep -v "#")
 cats=$(cat ../nom_WS/cats.cfg | grep -v "#" | grep ":" | cut -d ":" -f 1)
 
-basepath=/publicfs/atlas/atlasnew/higgs/hgg/chenhr/vbfcp/WSconfigs/configToys/
+cfgdir=configToys
+basepath=/publicfs/atlas/atlasnew/higgs/hgg/chenhr/vbfcp/WSconfigs/${cfgdir}/
 #exampath=${basepath}/configToy0/
 exampath=config/
 for i in ${itoy}
@@ -18,8 +19,8 @@ do
   echo "start substitution"
   for d in ${d_tilde}
   do
-    sed -i "s/\/publicfs\/atlas\/atlasnew\/higgs\/hgg\/chenhr\/vbfcp\/WSconfigs\/configToys\/configToy${i}//g" ${basepath}/configToy${i}/vbf_cp_${d}/d_tilde_${d}.xml
-    sed -i "s/config/\/publicfs\/atlas\/atlasnew\/higgs\/hgg\/chenhr\/vbfcp\/WSconfigs\/configToys\/configToy${i}/g" ${basepath}/configToy${i}/vbf_cp_${d}/d_tilde_${d}.xml
+    sed -i "s/\/publicfs\/atlas\/atlasnew\/higgs\/hgg\/chenhr\/vbfcp\/WSconfigs\/${cfgdir}\/configToy${i}//g" ${basepath}/configToy${i}/vbf_cp_${d}/d_tilde_${d}.xml
+    sed -i "s/config/\/publicfs\/atlas\/atlasnew\/higgs\/hgg\/chenhr\/vbfcp\/WSconfigs\/${cfgdir}\/configToy${i}/g" ${basepath}/configToy${i}/vbf_cp_${d}/d_tilde_${d}.xml
 
     sed -i "s/wsToy${i}//g" ${basepath}/configToy${i}/vbf_cp_${d}/d_tilde_${d}.xml
     sed -i "s/workspace/wsToy${i}/g" ${basepath}/configToy${i}/vbf_cp_${d}/d_tilde_${d}.xml
@@ -30,8 +31,8 @@ do
       fcat=${basepath}/configToy${i}/vbf_cp_${d}/channel/category_OO_${cat}.xml
       if [[ ${cat} =~ "TT" || ${cat} =~ "TL" ]];then sed -i "s/toy_${cat}_0/toy_${cat}_${i}/" ${fcat};fi
 
-      sed -i "s/\/publicfs\/atlas\/atlasnew\/higgs\/hgg\/chenhr\/vbfcp\/WSconfigs\/configToys\/configToy${i}//g" ${fcat}
-      sed -i "s/config/\/publicfs\/atlas\/atlasnew\/higgs\/hgg\/chenhr\/vbfcp\/WSconfigs\/configToys\/configToy${i}/g" ${fcat}
+      sed -i "s/\/publicfs\/atlas\/atlasnew\/higgs\/hgg\/chenhr\/vbfcp\/WSconfigs\/${cfgdir}\/configToy${i}//g" ${fcat}
+      sed -i "s/config/\/publicfs\/atlas\/atlasnew\/higgs\/hgg\/chenhr\/vbfcp\/WSconfigs\/${cfgdir}\/configToy${i}/g" ${fcat}
     done
   done
 done
